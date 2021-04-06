233cb14be68a12b1cbf18cd949d326b92e929ce4
范淼 <femiajayi705@gmail.com>
2021-04-06 23:36:21 +0800

    feat: Use product data in shop route

--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,12 @@
 const path = require("path");
-
+const { products } = require("./admin");
 const rootDir = require("../utils/getPath");
 const express = require("express");
 
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
+  console.log("The products :", products);
   res.sendFile(path.join(rootDir, "views", "shop.html"));
   //....
 });
