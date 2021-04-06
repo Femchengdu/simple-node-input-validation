0fa80bb7dbc363ca8edf2030ba690c1e57e27014
范淼 <femiajayi705@gmail.com>
2021-04-06 23:35:44 +0800

    feat: Added structure to handle data

--- routes/admin.js
+++ routes/admin.js
@@ -4,6 +4,9 @@ const express = require("express");
 const rootDir = require("../utils/getPath");
 const router = express.Router();
 
+// Products data store
+const products = [];
+
 router.get("/add-product", (req, res, next) => {
   res.sendFile(path.join(rootDir, "views", "add-product.html"));
   //....
@@ -11,6 +14,10 @@ router.get("/add-product", (req, res, next) => {
 
 router.post("/add-product", (req, res, next) => {
   console.log(req.body);
+  const { title } = req.body;
+  products.push({ title });
   res.redirect("/");
 });
-module.exports = router;
+module.exports = { router, products };
+
+//zz
