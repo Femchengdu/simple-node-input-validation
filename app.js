d125af05ea73bb89b3ab56a5bf2f0d3c2fd1cb55
范淼 <femiajayi705@gmail.com>
2021-04-09 05:34:29 +0800

    refactor: Remove unwanted code

--- app.js
+++ app.js
@@ -1,35 +1,15 @@
 const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
-
-//const exphbs = require("express-handlebars");
-
 // Import router middleware
-const { router: adminRoutes } = require("./routes/admin");
+const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const { pathError } = require("./controllers/error");
 
 // Create app
 const app = express();
 
 /** Templates */
-// Register the handlebars engine
-// app.engine(
-//   "handlebars",
-//   exphbs({
-//     extname: "handlebars",
-//     layoutsDir: "views/layout/", // Default location for handlebars
-//     defaultLayout: "main-layout",
-//     extname: "handlebars", // default is handlebars
-//   })
-// );
-// End register handlebars engine reg
-
-// Pug engine is builtin unlike handlebars above
-// using pug engine
-// app.set("view engine", "pug");
-
-// using handlebars engine
-//app.set("view engine", "handlebars");
 
 // using ejs
 app.set("view engine", "ejs");
@@ -40,22 +20,12 @@ app.set("views", "views");
 // Deprecation warning on bodyParser below.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+
+// Routes and controllers
 app.use("/admin", adminRoutes);
 
 app.use(shopRoutes);
 
-/** Render Templates */
-// setup 404 ..
-// HTML Template
-// app.use((req, res, next) => {
-//   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
-// });
-
-// Pug
-app.use((req, res, next) => {
-  res.status(404).render("404", { pageTitle: "Page Not Found!" });
-});
-
-/** End render Templates */
+app.use(pathError);
 
 app.listen(3090);
